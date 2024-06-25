e1df0c3e8c7131bcf703805a7cb18d630299ecf7
fakys <fakysgg@gmail.com>
2024-06-25 20:23:15 +0300

    update sale

--- backend/components/views/update/update_sales.php
+++ backend/components/views/update/update_sales.php
@@ -4,8 +4,16 @@
  */
 
 use yii\db\Query;
+use yii\helpers\ArrayHelper;
 use yii\widgets\ActiveForm;
 $group= (new Query())->select(['id', 'title'])->from('user_groups')->all();
+$products = \backend\models\Product::find()->asArray()->all();
+
+$product_sales = [];
+
+foreach ($model->getProducts()->asArray()->all() as $val){
+    $product_sales[$val['sale_id']] = ['Selected'=>'selected'];
+}
 
 ?>
 
@@ -23,6 +31,7 @@ $group= (new Query())->select(['id', 'title'])->from('user_groups')->all();
         <?php endif;?>
         <?= $form->field($model, 'icon')->fileInput(['class'=>'form-control', 'accept'=>'image/png, image/gif, image/jpeg'])?>
     </div>
+    <?=$form->field($model, 'products[]')->dropDownList(ArrayHelper::map($products, 'id', 'title'), ['multiple'=>true, 'options' => $product_sales])?>
     <?= $form->field($model, 'created_at')->input('datetime-local')?>
     <div><input class="btn btn-primary" type="submit" value="Создать"></div>
 <?php ActiveForm::end()?>
--- backend/models/Sale.php
+++ backend/models/Sale.php
@@ -6,6 +6,7 @@ use yii\base\Exception;
 use yii\behaviors\TimestampBehavior;
 use yii\db\ActiveRecord;
 use yii\db\Expression;
+use yii\helpers\ArrayHelper;
 use yii\web\UploadedFile;
 
 class Sale extends ActiveRecord
@@ -23,6 +24,7 @@ class Sale extends ActiveRecord
 
     public const CREATE  = 'create';
     public const UPDATE  = 'update';
+    public $products = [];
     public static function tableName()
     {
         return 'sales';
@@ -41,6 +43,7 @@ class Sale extends ActiveRecord
             ['title', 'string'],
             ['description', 'string', 'length'=>[10, 4000]],
             ['icon', 'image', 'extensions'=>'png, jpg, gif'],
+            ['products', 'safe'],
             ['created_at', 'datetime', 'format'=>'php:Y-m-d\TH:i:s']
         ];
     }
@@ -69,12 +72,48 @@ class Sale extends ActiveRecord
         }
     }
 
+    public function add_products()
+    {
+        $sale_product_id = ArrayHelper::getColumn($this->getProducts()->asArray()->all(), 'product_id');
+        if($this->products){
+            foreach ($this->products as $val){
+                if(!in_array($val, $sale_product_id)){
+                    (new ProductSale(['sale_id'=>$this->id, 'product_id'=>$val]))->save();
+                }
+            }
+        }
+    }
+    public function delete_sale()
+    {
+        $sale_product_id = ArrayHelper::getColumn($this->getProducts()->asArray()->all(), 'product_id');
+        if(!is_array($this->products)){
+            $this->products = [];
+        }
+        foreach ($sale_product_id as $val){
+            if(!in_array($val, $this->products)){
+                $sale_product = ProductSale::find()->where(['product_id'=> $val,'sale_id'=>$this->id])->one();
+                if($sale_product){
+                    $sale_product->delete();
+                }
+            }
+        }
+    }
+
 
     public function beforeSave($insert)
     {
         parent::beforeSave($insert);
+        if(!$insert){
+            $this->add_products();
+            $this->delete_sale();
+        }
         $this->add_icon();
 
         return true;
+    }
+
+    public function getProducts()
+    {
+        return $this->hasMany(ProductSale::class, ['sale_id'=>'id']);
     }
 }
