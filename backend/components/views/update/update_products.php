20105f39418d5554e71e3cfcd21b64f993ecbea3
fakys <fakysgg@gmail.com>
2024-06-25 19:57:57 +0300

    update products

---
+++ backend/components/views/update/update_products.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @var \yii\db\ActiveRecord $model
+ */
+
+use backend\components\CreateObjectsComponent;
+use yii\db\Query;
+use yii\helpers\ArrayHelper;
+use yii\widgets\ActiveForm;
+$categories= (new Query())->select(['id', 'title'])->from('categories')->all();
+$photos = $model->getPhotos()->asArray()->all();
+$sales = \backend\models\Sale::find()->all();
+$product_sales = [];
+
+foreach ($model->getSales()->asArray()->all() as $val){
+    $product_sales[$val['sale_id']] = ['Selected'=>'selected'];
+}
+?>
+
+
+<?php $form = ActiveForm::begin(['enableClientValidation' => false])?>
+    <?= $form->field($model, 'title')?>
+    <?= $form->field($model, 'description')->textarea()?>
+    <?=$form->field($model, 'price')->input('number')?>
+    <div>
+        <?php if($model->main_photo):?>
+            <label class="control-label">Старое фото</label>
+            <div class="image-container-update">
+                <img src="<?=Yii::getAlias('@frontendUrl')."/$model->main_photo"?>">
+            </div>
+        <?php endif;?>
+        <?=$form->field($model, 'main_photo')->fileInput(['class'=>'form-control', 'accept'=>'image/png, image/gif, image/jpeg'])?>
+    </div>
+    <div>
+        <?php if($photos):?>
+        <div class="images-scroll-block">
+            <?php foreach ($photos as $val):?>
+            <div class="images-container-update">
+                <img src="<?=Yii::getAlias('@frontendUrl')."/{$val['photo']}"?>">
+                <div>photo: <?=$val['photo']?></div>
+                <div><samp>id: <?=$val['id']?></samp></div>
+                <?=$form->field($model,"deleted_photos[{$val['id']}]")->checkbox()?>
+            </div>
+            <?php endforeach;?>
+        </div>
+        <?php endif;?>
+        <?=$form->field($model, 'photos[]')->fileInput(['class'=>'form-control', 'accept'=>'image/png, image/gif, image/jpeg', 'multiple'=>true])?>
+    </div>
+    <?=$form->field($model, 'category_id')->dropDownList(ArrayHelper::map($categories, 'id','title'))?>
+    <?=$form->field($model, 'sales[]')->dropDownList(ArrayHelper::map($sales, 'id', 'title'), ['multiple'=>true, 'options' => $product_sales])?>
+    <?= $form->field($model, 'created_at')->input('datetime-local')?>
+    <div><input class="btn btn-primary" type="submit" value="Создать"></div>
+<?php ActiveForm::end()?>
--- backend/models/Product.php
+++ backend/models/Product.php
@@ -2,9 +2,11 @@
 namespace backend\models;
 
 
+use Yii;
 use yii\behaviors\TimestampBehavior;
 use yii\db\ActiveRecord;
 use yii\db\Expression;
+use yii\helpers\ArrayHelper;
 use yii\web\UploadedFile;
 
 class Product extends ActiveRecord
@@ -19,6 +21,8 @@ class Product extends ActiveRecord
         ];
     }
     public $photos=[];
+    public $deleted_photos = [];
+    public $sales = [];
 
     public const CREATE  = 'create';
     public const UPDATE  = 'update';
@@ -31,10 +35,12 @@ class Product extends ActiveRecord
     public function rules()
     {
         return [
-            [['title', 'description', 'price'], 'required', 'on'=>'create'],
+            [['title', 'description', 'price'], 'required', 'on'=>[self::CREATE,self::UPDATE]],
+            [['title', 'description', 'price'], 'string'],
             [['main_photo', 'photos'], 'image', 'extensions'=>'png, jpg, gif'],
-            ['category_id', 'number'],
-            ['created_at', 'datetime', 'format'=>'php:Y-m-d\TH:i:s']
+            [['category_id'], 'number'],
+            ['created_at', 'datetime', 'format'=>'php:Y-m-d\TH:i:s', 'on'=>[self::UPDATE]],
+            [['deleted_photos', 'sales', 'created_at'], 'safe']
         ];
     }
     public static function tableLabel()
@@ -51,7 +57,9 @@ class Product extends ActiveRecord
             'main_photo'=>'Главное фото',
             'photos'=>'Фотографии',
             'category_id'=>'Категории',
-            'created_at'=>"Время создания"
+            'created_at'=>"Время создания",
+            'deleted_photos'=>'Удалить фотографию',
+            'sales'=>'Акции'
         ];
     }
     public function add_main_photo()
@@ -61,6 +69,8 @@ class Product extends ActiveRecord
         if($file){
             $this->main_photo = "storage/products_main_photo/{$file_name}.{$file->getExtension()}";
             $file->saveAs("@frontend/web/{$this->main_photo}");
+        }else{
+            unset($this->main_photo);
         }
     }
     public function add_photos()
@@ -76,14 +86,54 @@ class Product extends ActiveRecord
         }
     }
 
+    public function deleted_photos()
+    {
+        foreach ($this->deleted_photos as $id=>$val) {
+            if($val){
+                $photo = ProductPhoto::findOne($id);
+                if($photo){
+                    $photo->delete();
+                }
+            }
+        }
+    }
+    public function add_sales()
+    {
+        $sale_product_id = ArrayHelper::getColumn($this->getSales()->asArray()->all(), 'sale_id');
+        if($this->sales){
+            foreach ($this->sales as $val){
+                 if(!in_array($val, $sale_product_id)){
+                     (new ProductSale(['sale_id'=>$val, 'product_id'=>$this->id]))->save();
+                 }
+            }
+        }
+    }
+    public function delete_sale()
+    {
+        $sale_product_id = ArrayHelper::getColumn($this->getSales()->asArray()->all(), 'sale_id');
+        if(!is_array($this->sales)){
+            $this->sales = [];
+        }
+        foreach ($sale_product_id as $val){
+            if(!in_array($val, $this->sales)){
+                $product = ProductSale::find()->where(['product_id'=>$this->id, 'sale_id'=>$val])->one();
+                if($product){
+                    $product->delete();
+                }
+            }
+        }
+    }
     public function beforeSave($insert)
     {
         parent::beforeSave($insert);
-        if($insert){
-            $this->add_main_photo();
-        }else{
-
+        if(!$insert){
+            $this->deleted_photos();
+            $this->add_photos();
+            $this->add_sales();
+            $this->delete_sale();
         }
+        $this->deleted_photos();
+        $this->add_main_photo();
         return true;
     }
     public function afterSave($insert, $changedAttributes)
@@ -95,5 +145,14 @@ class Product extends ActiveRecord
 
         }
         return true;
+    }
+
+    public function getPhotos()
+    {
+        return $this->hasMany(ProductPhoto::class, ['product_id'=>'id']);
+    }
+    public function getSales()
+    {
+        return $this->hasMany(ProductSale::class, ['product_id'=>'id']);
     }
 }
