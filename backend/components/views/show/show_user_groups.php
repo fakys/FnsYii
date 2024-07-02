d3a2f0d458d93053cdfcad39a92cb4687f7e4562
fakys <fakysgg@gmail.com>
2024-07-02 12:52:42 +0300

    create show user_groups

--- backend/components/views/show/show_user_groups.php
+++ backend/components/views/show/show_user_groups.php
@@ -1,5 +1,8 @@
 <?php
-    $users = $model->getUsers()->all();
+
+use yii\helpers\Url;
+
+$users = $model->getUsers()->all();
 ?>
 <div>
     <div class="card mb-3">
@@ -8,9 +11,20 @@
             <p class="card-text font-size-20"><?=$model->title?></p>
         </div>
     </div>
+    <?php if($model->icon):?>
+    <div class="card mb-3">
+        <div class="card-header text-white  bg-dark">Фото</div>
+        <div class="card-body">
+            <div class="image-container">
+                <img src='<?=Yii::getAlias("@frontendUrl")."/$model->icon"?>'>
+            </div>
+        </div>
+    </div>
+    <?php endif;?>
+    <?php if($users):?>
     <div class="card mb-3">
         <div class="card-header text-white  bg-dark">Пользователи</div>
-        <div class="card-body">
+        <div class="card-body show-scroll-block">
             <?php foreach ($users as $user):?>
                 <div class="d-flex">
                     <div class="content-scroll-block">
@@ -19,10 +33,27 @@
                         <div><?=$user->created_at?></div>
                     </div>
                     <div class="ml-auto">
-                        <div class="btn btn-danger p-1">Удалить</div>
+                        <a href="<?=Url::to(['admin/update', 'table'=>$user::tableName(), 'id'=>$user->id])?>" class="btn btn-success p-1">Изменить</a>
                     </div>
                 </div>
             <?php endforeach;?>
         </div>
     </div>
+    <?php endif;?>
+    <?php if($model->created_at):?>
+        <div class="card mb-3">
+            <div class="card-header text-white  bg-dark">Дата создания</div>
+            <div class="card-body">
+                <?=$model->created_at?>
+            </div>
+        </div>
+    <?php endif;?>
+    <?php if($model->updated_at):?>
+        <div class="card mb-3">
+            <div class="card-header text-white  bg-dark">Дата обновления</div>
+            <div class="card-body">
+                <?=$model->updated_at?>
+            </div>
+        </div>
+        <?php endif;?>
 </div>
--- backend/views/admin/show_object.php
+++ backend/views/admin/show_object.php
@@ -1,4 +1,10 @@
 <?php
 use backend\components\ShowObjectComponent;
+use yii\helpers\Url;
+
 ?>
 <?=ShowObjectComponent::widget(['model'=>$model])?>
+<div class="mt-4">
+    <a href="<?=Url::to(['admin/delete', 'table'=>$model::tableName(), 'id'=>$model->id])?>" class="btn btn-danger">Удалить</a>
+    <a href="<?=Url::to(['admin/update', 'table'=>$model::tableName(), 'id'=>$model->id])?>" class="btn btn-success">Изменить</a>
+</div>
